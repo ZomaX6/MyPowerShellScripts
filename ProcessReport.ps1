f4658128b1bfdde8bd66813aba61b8babc136b9a
ZomaX6 <44568606+ZomaX6@users.noreply.github.com>
2018-10-31 21:35:43 +0100

    ver 1.0.1
    Report.csv > Report1.csv

--- ProcessReport.ps1
+++ ProcessReport.ps1
@@ -1,7 +1,7 @@
 
 <#PSScriptInfo
 
-.VERSION 1.0.0
+.VERSION 1.0.1
 
 .GUID 937de923-8b15-46b4-8db1-c47b7edc7344
 
@@ -40,4 +40,4 @@
 Param()
 
 
-Get-Process | Select-Object Name, Id, @{n="WorkingSet"; e={[math]::Round(($_.WS / 1MB), 2)}} | Sort-Object WorkingSet -Descending | Export-Csv C:\Scripts\Report.csv -NoTypeInformation
+Get-Process | Select-Object Name, Id, @{n="WorkingSet"; e={[math]::Round(($_.WS / 1MB), 2)}} | Sort-Object WorkingSet -Descending | Export-Csv C:\Scripts\Report1.csv -NoTypeInformation
